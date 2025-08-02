8445374a0570c2fc502e569bfbb01d7244748b3c
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-08-02 23:42:42 +0530

    Add updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className=" bg-gray-900 text-white py-6 mt-5">
-      <div className="max-w-7xl mx-auto px-4 flex flex-col sm:flex-row items-center justify-between gap-4">
+    <footer className=" bg-blue-300 text-white py-6 mt-0 md:w-full text-center">
+      <div className="max-w-7xl mx-auto px-2 md:px-118 flex flex-col sm:flex-row items-center justify-between gap-4">
         <p className="text-sm text-center sm:text-left">
           &copy; {new Date().getFullYear()}{" "}
           <span className="font-semibold">Saifuddin Khan</span>. All rights reserved.
